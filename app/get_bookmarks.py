1c174ae8a6e7ea4fe6fc1c2ea5027918bd7e63e5
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-31 10:37:56 -0400

    OK this test request is successful at least

--- app/get_bookmarks.py
+++ app/get_bookmarks.py
@@ -6,23 +6,22 @@ import requests
 BASE_URL = "https://tuner.pandora.com/services/json"
 
 def bookmarks_url():
-    method = "user.getBookmarks"
-    request_url = os.path.join(BASE_URL, f"?method={method}")
-
-    partner_id = "ABC" # TODO: https://6xq.net/pandora-apidoc/json/authentication/#auth-partnerlogin
-    request_url += f"&partner_id={partner_id}"
-
-    auth_token = "TODO" # TODO: https://6xq.net/pandora-apidoc/json/authentication/#auth-userlogin
-    request_url += f"&userAuthToken={auth_token}"
-
-    current_time = int(round(time.time() * 1000))
-    diff = 0 # TODO: (time of Partner login request – syncTime from Partner login response)
-    sync_time = current_time + diff
-    request_url += f"&syncTime={sync_time}"
-
+    request_url = f"{BASE_URL}/?method=user.getBookmarks"
+    #
+    #partner_id = "ABC" # TODO: https://6xq.net/pandora-apidoc/json/authentication/#auth-partnerlogin
+    #request_url += f"&partner_id={partner_id}"
+    #
+    #auth_token = "TODO" # TODO: https://6xq.net/pandora-apidoc/json/authentication/#auth-userlogin
+    #request_url += f"&userAuthToken={auth_token}"
+    #
+    #current_time = int(round(time.time() * 1000))
+    #diff = 0 # TODO: (time of Partner login request – syncTime from Partner login response)
+    #sync_time = current_time + diff
+    #request_url += f"&syncTime={sync_time}"
+    #
     return request_url
 
-request_url = bookmarks_url()
+request_url = f"{BASE_URL}/?method=test.checkLicensing"
 
 print("REQUEST URL:", request_url)
 
