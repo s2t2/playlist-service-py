b2fe95029b60f2ba74dad84b6cee602220c48ea3
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-31 14:44:53 -0400

    Simplify, loop through all bookmarks

--- app/get_bookmarks2.py
+++ app/get_bookmarks2.py
@@ -7,10 +7,8 @@
 
 import os
 import pprint
-
 from dotenv import load_dotenv
-import pydora # source of "pandora" sub-package
-#from pandora.client import APIClient
+import pydora # allows importing of the "pandora" sub-package
 import pandora.clientbuilder as cb
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -21,58 +19,34 @@ PANDORA_USERNAME = os.environ.get("PANDORA_USERNAME", "OOPS")
 PANDORA_PASSWORD = os.environ.get("PANDORA_PASSWORD", "OOPS")
 
 client_settings = {
-    # tuner.pandora.com
-    "android": {
-        "DECRYPTION_KEY": "R=U!LH$O2B#",
-        "ENCRYPTION_KEY": "6#26FRL$ZWD",
-        "PARTNER_USER": "android",
-        "PARTNER_PASSWORD": "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7",
-        "DEVICE": "android-generic"
-    },
-    "ios": {
-        "DECRYPTION_KEY": "20zE1E47BE57$51",
-        "ENCRYPTION_KEY": "721^26xE22776",
-        "PARTNER_USER": "iphone",
-        "PARTNER_PASSWORD": "P2E4FC0EAD3*878N92B2CDp34I0B1@388137C",
-        "DEVICE": "IP01"
-    },
-    # internal-tuner.pandora.com
-    "desktop": {
-        "DECRYPTION_KEY": "U#IO$RZPAB%VX2",
-        "ENCRYPTION_KEY": "2%3WCL*JU$MP]4",
-        "PARTNER_USER": "pandora one",
-        "PARTNER_PASSWORD": "TVCKIBGS9AO9TSYLNNFUML0743LH82D",
-        "DEVICE": "D01",
+    "DECRYPTION_KEY": "R=U!LH$O2B#",
+    "ENCRYPTION_KEY": "6#26FRL$ZWD",
+    "PARTNER_USER": "android",
+    "PARTNER_PASSWORD": "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7",
+    "DEVICE": "android-generic"
+} # FYI: these are generic device settings not personal private info (see: https://6xq.net/pandora-apidoc/json/partners/#partners)
+client = cb.SettingsDictBuilder(client_settings).build()
+print("---------------------------")
+print("CLIENT:", type(client)) #> <class 'pandora.client.APIClient'>
+pp.pprint(dir(client))
+
+print("---------------------------")
+print("LOGGING IN...")
+login_response = client.login(PANDORA_USERNAME, PANDORA_PASSWORD)
+pp.pprint(login_response)
+
+print("---------------------------")
+print("GETTING BOOKMARKS...")
+bookmarks_response = client.get_bookmarks()
+print(type(bookmarks_response))
+pp.pprint(dir(bookmarks_response))
+
+print(f"FOUND {len(bookmarks_response.songs)} BOOKMARKS:")
+for song in bookmarks_response.songs:
+    song_info = {
+        "title": song.song_name,
+        "artist": song.artist_name,
+        "album": song.album_name,
+        "bookmarked_on": song.date_created.strftime("%Y-%m-%d")
     }
-}
-
-settings = client_settings["android"]
-
-client = cb.SettingsDictBuilder(settings).build() #> <class 'pandora.client.APIClient'>
-# pp.pprint(dir(client))
-
-breakpoint()
-
-#client.login("username", "password") #> invalid partner login
-client.login(PANDORA_USERNAME, PANDORA_PASSWORD)
-#> {
-#   'hasAudioAds': False,
-#   'isCapped': False,
-#   'username': '______',
-#   'canListen': True,
-#   'subscriptionHasExpired': False,
-#   'userId': '1234567',
-#   'listeningTimeoutMinutes': '1440',
-#   'zeroVolumeNumMutedTracks': 1,
-#   'zeroVolumeAutoPauseEnabledFlag': True,
-#   'maxStationsAllowed': 250,
-#   'listeningTimeoutAlertMsgUri': '/mobile/still_listening.vm',
-#   'userProfileUrl': 'https://www.pandora.com/login?auth_token=ABC123&target=DEF456',
-#   'userAuthToken': '____________'
-# }
-
-response = client.get_bookmarks()
-print(type(response)) #> <class 'pandora.models.pandora.BookmarkList'>
-print(type(response.songs)) #> <class 'list'>
-
-len(response.songs) #> 83
+    pp.pprint(song_info)
