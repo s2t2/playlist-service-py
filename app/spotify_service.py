fb1afe2a256d97dbb63faba051c88dcaabaaf91e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-06 23:20:11 -0400

    Adapt playlist pagination example

--- app/spotify_service.py
+++ app/spotify_service.py
@@ -14,19 +14,42 @@ if __name__ == "__main__":
     client_credentials_manager = SpotifyClientCredentials() # implicitly uses SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET env vars!!
     client = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     print("---------------")
-    print("CLIENT:", type(client), dir(client))
+    print("CLIENT:", type(client)) #> <class 'spotipy.client.Spotify'>
+    print(dir(client))
 
-    search_term = "Springsteen on Broadway"
-    print("---------------")
-    print("SEARCH:", search_term)
+    #
+    # EXAMPLE SEARCH
+    #
+    #search_term = "Springsteen on Broadway"
+    #print("---------------")
+    #print("SEARCH:", search_term)
+#
+    #response = client.search(q=search_term, limit=20)
+    #print("---------------")
+    #print("RESPONSE:", type(response))
+#
+    #songs = response["tracks"]["items"]
+    #print("---------------")
+    #print(f"SONGS ({len(songs)}):", type(response))
+#
+    #for i, song in enumerate(songs):
+    #    print(f"  {i}) {song['name']}")
 
-    response = client.search(q=search_term, limit=20)
-    print("---------------")
-    print("RESPONSE:", type(response))
 
-    songs = response["tracks"]["items"]
-    print("---------------")
-    print(f"SONGS ({len(songs)}):", type(response))
+    #
+    # GET PLAYLISTS
+    # ... adapted from: https://spotipy.readthedocs.io/en/latest/#client-credentials-flow
+    #
 
-    for i, song in enumerate(songs):
-        print(f"  {i}) {song['name']}")
+    playlists = client.user_playlists("spotify") # hmm there are > 900 playlists and I needed to quit the program... need to try a different request
+
+    while playlists:
+        print(type(playlists))
+
+        for i, playlist in enumerate(playlists['items']):
+            print(f"{i + 1 + playlists['offset']} {playlist['uri']} {playlist['name']}")
+
+        if playlists["next"]:
+            playlists = client.next(playlists)
+        else:
+            playlists = None
