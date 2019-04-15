4c5afbcd682d2c0cbd2a4050021607b2b2758014
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-14 22:29:18 -0400

    It's alive

--- app/sync_service.py
+++ app/sync_service.py
@@ -1,22 +1,41 @@
 
-from spotify_service import find_or_create_playlist
 from pandora_service import get_bookmarks
+from spotify_service import find_or_create_playlist, song_search, add_tracks
 
 if __name__ == "__main__":
 
-    playlist = find_or_create_playlist() # TODO: consider getting all playlists and asking the user to choose one
+    # RESET PLAYLIST
+
+    playlist = find_or_create_playlist() # TODO: destroy_and_create_playlist()
+    print("--------------------------------")
     print("PLAYLIST: ", playlist["id"], playlist["name"])
 
-    songs = get_bookmarks()
-    print(f"BOOKMARKS ({len(songs)}):")
-    for song in songs:
-        print({
-            "bookmarked_on": song.date_created.strftime("%Y-%m-%d"),
-            "artist": song.artist_name,
-            "title": song.song_name,
-            #"album": song.album_name
-        })
+    playlist_id = playlist["id"]
 
-        #if song.song_name not in playlist_song_names:
-        #    parsed_response = add_tracks(playlist_id, track_uris)
-        #    print("ADDED TRACKS, SNAPSHOT:", parsed_response["snapshot_id"])
+    # LIST BOOKMARKED SONGS
+
+    songs = get_bookmarks()
+    print("--------------------------------")
+    print(f"BOOKMARKS ({len(songs)}):")
+    for song in songs[0:2]:
+
+        # SEARCH FOR MATCHING SONG
+
+        search_term = f"{song.song_name} {song.artist_name}"
+        print("--------------")
+        print(f"SONG SEARCH TERM: {search_term}")
+
+        tracks = song_search(search_term)
+        print(f"SEARCH RESULTS ({len(tracks)}):")
+        for track in tracks:
+            print(f"  + {track['uri']} '{track['name']}' by {track['artists'][0]['name']}")
+
+        track = tracks[0] # consider allowing user to select
+        print("SELECTED TRACK:", track["uri"])
+
+        track_ids = [track["uri"]]
+
+        # ADD SONG TO PLAYLIST
+
+        parsed_response = add_tracks(playlist_id, track_ids)
+        print("ADDED TRACK, SNAPSHOT:", parsed_response["snapshot_id"])
