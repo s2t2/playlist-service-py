513863abe6d549c1bef950375147d652b5ff58ab
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-14 22:34:16 -0400

    Handle song not found error

--- app/sync_service.py
+++ app/sync_service.py
@@ -17,25 +17,30 @@ if __name__ == "__main__":
     songs = get_bookmarks()
     print("--------------------------------")
     print(f"BOOKMARKS ({len(songs)}):")
-    for song in songs[0:2]:
+    for song in songs:
 
         # SEARCH FOR MATCHING SONG
 
         search_term = f"{song.song_name} {song.artist_name}"
-        print("--------------")
-        print(f"SONG SEARCH TERM: {search_term}")
+        print("--------------------------------")
+        print(f"SONG SEARCH TERM: '{search_term}'")
 
         tracks = song_search(search_term)
         print(f"SEARCH RESULTS ({len(tracks)}):")
+
         for track in tracks:
             print(f"  + {track['uri']} '{track['name']}' by {track['artists'][0]['name']}")
 
-        track = tracks[0] # consider allowing user to select
-        print("SELECTED TRACK:", track["uri"])
+        try:
+            track = tracks[0] # consider allowing user to select
+            print("SELECTED:", track["uri"])
 
-        track_ids = [track["uri"]]
+            track_ids = [track["uri"]]
 
-        # ADD SONG TO PLAYLIST
+            # ADD SONG TO PLAYLIST
 
-        parsed_response = add_tracks(playlist_id, track_ids)
-        print("ADDED TRACK, SNAPSHOT:", parsed_response["snapshot_id"])
+            #parsed_response = add_tracks(playlist_id, track_ids)
+            #print("... ADDED TRACK, SNAPSHOT:", parsed_response["snapshot_id"])
+
+        except IndexError as e:
+            print("N/A - NONE FOUND")
