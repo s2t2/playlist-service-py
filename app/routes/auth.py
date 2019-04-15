bfd3b94aa325e032ac4e34cea847d72bc21214c0
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-15 17:37:56 -0400

    WIP - This is still prompting for a terminal input

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,3 +1,15 @@
+#import os
+#from dotenv import load_dotenv
+import spotipy
+#from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import spotipy.util as util
+#load_dotenv()
+from app.spotify_service import USERNAME, AUTH_SCOPE
+
+
+
+
+
 from flask import Blueprint, request, render_template
 
 auth_routes = Blueprint("auth_routes", __name__)
@@ -8,10 +20,17 @@ def index():
     return "You have visited the homepage"
     #return render_template("index.html")
 
+@auth_routes.route("/auth/spotify/login")
+def spotify_login():
+    print("LOGGING IN TO SPOTIFY...")
+    token = util.prompt_for_user_token(USERNAME, AUTH_SCOPE)
+    return f"THE TOKEN IS: {token}"
+    #return render_template("index.html")
+
 # GET /auth/spotify/callback
 # GET /auth/spotify/callback?code=xyz123
 @auth_routes.route("/auth/spotify/callback")
-def hello(code=None):
+def spotify_callback(code=None):
     print("CALLBACK")
     print("REQUEST PARAMS:", dict(request.args))
 
