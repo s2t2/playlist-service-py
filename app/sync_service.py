9b478e9fd91cb2b0b6e73d3144e5c73819b849eb
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-30 00:11:21 -0400

    Sync Service (#5)
    * Refactor and test function to get bookmarks
    
    * It's alive
    
    * Handle song not found error
    
    * Sync service
    
    * Delete sync_test.py

---
+++ app/sync_service.py
@@ -0,0 +1,46 @@
+
+from pandora_service import get_bookmarks
+from spotify_service import find_or_create_playlist, song_search, add_tracks
+
+if __name__ == "__main__":
+
+    # RESET PLAYLIST
+
+    playlist = find_or_create_playlist() # TODO: destroy_and_create_playlist()
+    print("--------------------------------")
+    print("PLAYLIST: ", playlist["id"], playlist["name"])
+
+    playlist_id = playlist["id"]
+
+    # LIST BOOKMARKED SONGS
+
+    songs = get_bookmarks()
+    print("--------------------------------")
+    print(f"BOOKMARKS ({len(songs)}):")
+    for song in songs:
+
+        # SEARCH FOR MATCHING SONG
+
+        search_term = f"{song.song_name} {song.artist_name}"
+        print("--------------------------------")
+        print(f"SONG SEARCH TERM: '{search_term}'")
+
+        tracks = song_search(search_term)
+        print(f"SEARCH RESULTS ({len(tracks)}):")
+
+        for track in tracks:
+            print(f"  + {track['uri']} '{track['name']}' by {track['artists'][0]['name']}")
+
+        try:
+            track = tracks[0] # consider allowing user to select
+            print("SELECTED:", track["uri"])
+
+            track_ids = [track["uri"]]
+
+            # ADD SONG TO PLAYLIST
+
+            parsed_response = add_tracks(playlist_id, track_ids)
+            print(f"ADDED! SNAPSHOT:", parsed_response["snapshot_id"])
+
+        except IndexError as e:
+            print("N/A - NONE FOUND")
