7d0d5b422a5e23e2da25dcf90ac105b7ecf5abba
Mike Rossetti <s2t2@users.noreply.github.com>
2019-12-30 01:14:50 -0500

    Skip test on CI

--- test/export_pandora_bookmarks_test.py
+++ test/export_pandora_bookmarks_test.py
@@ -1,10 +1,13 @@
+import pytest
 
 from datetime import datetime
-
 from pandora.models.bookmark import Bookmark
 
 from app.exports.pandora.bookmarks import to_row
 
+from conftest import CI_ENV, SKIP_REASON
+
+@pytest.mark.skipif(CI_ENV==True, reason=SKIP_REASON)
 def test_to_row(pandora_bookmark):
     assert isinstance(pandora_bookmark, Bookmark)
 
